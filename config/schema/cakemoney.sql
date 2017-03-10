793113e91d60f90c924cd5e40fc20057ed1c60f9
Takeshi Nakamura <taqueci.n@gmail.com>
2017-03-10 23:57:07 +0900

    Update document

---
+++ config/schema/cakemoney.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `account` int(11) NOT NULL,
+  `description` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+);
+
+CREATE TABLE `journals` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `debit_id` int(11) NOT NULL,
+  `credit_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `summary` varchar(255) NOT NULL,
+  `description` text,
+  `asset` int(11) NOT NULL,
+  `liability` int(11) NOT NULL,
+  `income` int(11) NOT NULL,
+  `expense` int(11) NOT NULL,
+  `equity` int(11) NOT NULL,
+  `year` int(11) NOT NULL,
+  `month` int(11) NOT NULL,
+  `day` int(11) NOT NULL,
+  `week` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+);
